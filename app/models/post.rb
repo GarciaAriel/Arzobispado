e8014ca8c5d974367b12bd1a0e1cdfcb2195d8ff
araoznelson <nelsonaraoz@ymail.com>
2014-06-25 21:30:27 -0400

    paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
 
-	has_attached_file :image, styles: { medium: '200x200>', thumb: '48x48>' }
-	do_not_validate_attachment_file_type :image
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
