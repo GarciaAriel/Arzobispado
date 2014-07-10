b96ec80094db5ad99165306362b30e1965a984d0
garciaariel <ags123123@gmail.com>
2014-07-10 00:18:01 -0400

    usuarios

--- app/controllers/eventos_controller.rb
+++ app/controllers/eventos_controller.rb
@@ -1,13 +1,20 @@
 class EventosController < ApplicationController
+
 	before_action :set_evento, only: [:show, :edit, :update, :destroy]
+	skip_before_filter :verify_authenticity_token
 	def index
 		@submenu=1
 		@eventos = Evento.all
+		
 	end
 	
 	def new
-		@submenu=1
-		@evento = Evento.new
+		if current_user != nil
+			@submenu=1
+			@evento = Evento.new
+		else
+			redirect_to new_user_session_path
+		end
 	end
 
 	def show
@@ -16,33 +23,44 @@ class EventosController < ApplicationController
 	end
 
 	def edit
-		@submenu=1
-		@evento = Evento.find(params[:id])
+		if current_user != nil
+			@submenu=1
+			@evento = Evento.find(params[:id])
+		else
+			redirect_to new_user_session_path
+		end
 	end
 	
 	def create
-		@submenu=1
-	    @evento = Evento.new(evento_params)
+		if current_user != nil
+			@submenu=1
+		    @evento = Evento.new(evento_params)
 
-	    respond_to do |format|
-	      if @evento.save
-	        format.html { redirect_to @evento, notice: 'High score was successfully created.' }
-	      else
-	        format.html { render :new }
-	      end
-	    end
-
+		    respond_to do |format|
+		      if @evento.save
+		        format.html { redirect_to @evento, notice: 'High score was successfully created.' }
+		      else
+		        format.html { render :new }
+		      end
+		    end
+	    else
+			redirect_to new_user_session_path
+		end
   	end
 
   	def update
-  		@submenu=1
-  		respond_to do |format|
-  			if @evento.update(evento_params)
-  				format.html{redirect_to @evento, notice: 'evento ok'}
-  			else
-  				format.html {render :edit}	
-  			end
-  		end
+  		if current_user != nil
+	  		@submenu=1
+	  		respond_to do |format|
+	  			if @evento.update(evento_params)
+	  				format.html{redirect_to @evento, notice: 'evento ok'}
+	  			else
+	  				format.html {render :edit}	
+	  			end
+	  		end
+	    else
+			redirect_to new_user_session_path
+		end		
   	end
 
 
