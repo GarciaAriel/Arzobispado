3b04e682335739db3b6073b1dfd764af124dfa04
araoznelson <nelsonaraoz@ymail.com>
2014-07-22 16:33:59 -0400

    cambios heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Arzobispado.application.configure do
+Arzobispado::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Arzobispado.application.configure do
+Arzobispado::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
